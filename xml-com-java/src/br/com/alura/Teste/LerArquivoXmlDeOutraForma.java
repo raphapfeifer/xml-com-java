55c763c931460e98e82a7e06256d45227362fb07
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2017-08-30 00:50:36 -0300

    30/08/2017

---
+++ xml-com-java/src/br/com/alura/Teste/LerArquivoXmlDeOutraForma.java
@@ -0,0 +1,21 @@
+package br.com.alura.Teste;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+public class LerArquivoXmlDeOutraForma {
+	
+	public static void main(String[] args) throws Exception {
+		XMLReader leitor = XMLReaderFactory.createXMLReader();
+		LeitorXml logica = new LeitorXml();
+		leitor.setContentHandler(logica);
+		InputStream ips = new FileInputStream("src/vendasVideo3.xml");
+		InputSource is = new InputSource(ips);
+		leitor.parse(is);
+	}
+
+}
