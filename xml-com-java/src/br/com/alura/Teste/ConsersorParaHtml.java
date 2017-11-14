948e1b796a0124908abca0d1e56468c6c5166dee
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2017-11-14 02:36:11 -0200

    14/11/2017

---
+++ xml-com-java/src/br/com/alura/Teste/ConsersorParaHtml.java
@@ -0,0 +1,24 @@
+package br.com.alura.Teste;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+public class ConsersorParaHtml {
+	public static void main(String[] args) throws Exception {
+		InputStream xsl = new FileInputStream("src/xmlParaHtml.xsl");
+		StreamSource xslSource = new StreamSource(xsl);
+		
+		InputStream dados = new FileInputStream("src/vendasVideo3.xml");
+		StreamSource dadosSource = new StreamSource(dados);
+		
+		Transformer transformer = TransformerFactory.newInstance().newTransformer(xslSource);
+		
+		StreamResult saida = new StreamResult("src/vendas.html");
+		transformer.transform(dadosSource, saida);
+	}
+}
